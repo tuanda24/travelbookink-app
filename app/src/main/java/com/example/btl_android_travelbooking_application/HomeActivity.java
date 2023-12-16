71df3f21193e53bb3ccd6b6ed757fab925af3a62
tuanda24 <dat.2000.dz@gmail.com>
2023-12-16 15:17:16 +0700

    'saveupdate'

---
+++ app/src/main/java/com/example/btl_android_travelbooking_application/HomeActivity.java
@@ -0,0 +1,42 @@
+package com.example.btl_android_travelbooking_application;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_layout);
+        Button btnSub1 = findViewById(R.id.app_name1);
+        Button btnSub2=findViewById(R.id.app_name2);
+        Button btnCls=findViewById(R.id.cls_app);
+        btnCls.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+//        btnSub1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent1 = new Intent(MainScreenActivity.this, SubActivity1.class);
+//                startActivity(intent1);
+//            }
+//        });
+//
+//        btnSub2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent2 = new Intent(MainScreenActivity.this, SubActivity1.class);
+//                startActivity(intent2);
+//            }
+//        });
+    }
+}
--- app/src/main/java/com/example/btl_android_travelbooking_application/MainActivity.java
+++ app/src/main/java/com/example/btl_android_travelbooking_application/MainActivity.java
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
+=======
+
+>>>>>>> 5c953be (update)
 package com.example.btl_android_travelbooking_application;
 
 import androidx.appcompat.app.AppCompatActivity;
